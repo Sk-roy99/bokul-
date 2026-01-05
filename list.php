1c017b519290a69cb8b7754c3e2f07accf241864
BOKUL BORMON <sopnooroy738@gmail.com>
2026-01-05 14:12:54 +0530

    Create list.php

---
+++ list.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Inorder list</h3>
+    <ul type="circle">
+        <li>CE</li>
+        <li>ME</li>
+        <li>IT</li>
+        <li>EE</li>
+        <li>CSE</li>
+    </ul>
+    <ul>
+    <li>A</li>
+        <li>B</li>
+        <li>C</li>
+        <li>D</li>
+        <li>E</li>
+    </ul>
+    <h3>Order list</h3>
+    <ol type='A'>
+        <li>A</li>
+        <li>B</li>
+        <li>C</li>
+        <li>D</li>
+        <li>E</li>
+    </ol>
+    <ol type='a'>
+        <li>A</li>
+        <li>B</li>
+        <li>C</li>
+        <li>D</li>
+        <li>E</li>
+    </ol>
+    <ol>
+        <li>A</li>
+        <li>B</li>
+        <li>C</li>
+        <li>D</li>
+        <li>E</li>
+    </ol>
+</body>
+</html>
